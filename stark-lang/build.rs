722de306f1e513b6479553e3d2781102a7b3d417
Gilles Grousset <gilles.grousset@insideapp.fr>
2022-06-05 01:01:19 +0200

    refact: restored dylib for stark runtime

--- stark-lang/build.rs
+++ stark-lang/build.rs
@@ -12,11 +12,11 @@ fn main() {
     println!("cargo:rustc-link-search=native={}", dst.display());
 
     // For the moment: runtime is dynamically linked to prevent symbol stripping
-    //println!("cargo:rustc-link-lib=dylib=stark");
+    println!("cargo:rustc-link-lib=dylib=stark");
     // At sometime it whould be interesting to switch to static linking
     // Using the --whole-archive linker flag (seems to be not supported yet)
-    println!("cargo:rustc-link-lib=static=stark");
-    println!("cargo:rustc-link-arg=static=-Wl,–whole-archive -lstark");
+    //println!("cargo:rustc-link-lib=static=stark");
+    //println!("cargo:rustc-link-arg=static=-Wl,–whole-archive -lstark");
     //
 }
 //-rdynamic
