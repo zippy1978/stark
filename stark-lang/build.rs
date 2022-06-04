3f58df8aa6378b6299d269189f03f88399cbd7c7
Gilles Grousset <gilles.grousset@insideapp.fr>
2022-06-04 23:51:49 +0200

    fix: link with -rdynamic

--- stark-lang/build.rs
+++ stark-lang/build.rs
@@ -4,8 +4,9 @@ extern crate lalrpop;
 
 fn main() {
     lalrpop::process_root().unwrap();
-    
+
     let dst = Config::new("../stark-runtime").build();
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=dylib=stark");
 }
+//-rdynamic
